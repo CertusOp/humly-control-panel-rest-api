547cdfe581d5e2806a0ac0cff171f26d199e0c23
Eldar Kapetanovic <eldar.kapetanovic@softhouse.se>
2021-12-08 10:09:34 +0100

    Description for Devices endpoint added. Desks, Devices, and Wayfinding endpoints added to postman collection. Added Axios examples for Desk and Devices endpoints.

---
+++ REACT example files/desksResources.js
@@ -0,0 +1,61 @@
+import Axios from "axios";
+
+export default class DesksResource {
+    API_URL = "https://localhost:3002/api/v1";
+
+    getAllDesks(userId, authToken, queryParams) {
+        const requestOptions = {
+            headers: {
+                "X-User-Id": userId,
+                "X-Auth-Token": authToken,
+            },
+            params: {
+                country: queryParams.country,
+                city: queryParams.city,
+                building: queryParams.building,
+                floor: queryParams.floor,
+                date: queryParams.date,
+                status: queryParams.status,
+                pageNumber: queryParams.pageNumber,
+                pageSize: queryParams.pageSize,
+                sort: queryParams.sort,
+            },
+        };
+
+        return Axios.get(
+            `${this.API_URL}/desks`,
+            requestOptions
+        ).then(response => (
+            { responseStatus: response.status, responseData: response.data }
+        )).catch((error) => {
+            const errorResponse = {
+                responseStatus: error.response.status,
+                responseData: error.response.data,
+            };
+            throw errorResponse;
+        });
+    }
+
+    getDesk(userId, authToken, deskId) {
+        const requestOptions = {
+            headers: {
+                "Content-Type": "application/json",
+                "X-User-Id": userId,
+                "X-Auth-Token": authToken,
+            },
+        };
+
+        return Axios.get(
+            `${this.API_URL}/desks/${deskId}`,
+            requestOptions
+        ).then(response => (
+            { responseStatus: response.status, responseData: response.data }
+        )).catch((error) => {
+            const errorResponse = {
+                responseStatus: error.response.status,
+                responseData: error.response.data,
+            };
+            throw errorResponse;
+        });
+    }
+}
---
+++ REACT example files/devicesResources.js
@@ -0,0 +1,56 @@
+import Axios from "axios";
+
+export default class DevicesResource {
+    API_URL = "https://localhost:3002/api/v1";
+
+    getAllDevices(userId, authToken, queryParams) {
+        const requestOptions = {
+            headers: {
+                "X-User-Id": userId,
+                "X-Auth-Token": authToken,
+            },
+            params: {
+                status: queryParams.status,
+                pageNumber: queryParams.pageNumber,
+                pageSize: queryParams.pageSize,
+                sort: queryParams.sort,
+            },
+        };
+
+        return Axios.get(
+            `${this.API_URL}/devices`,
+            requestOptions
+        ).then(response => (
+            { responseStatus: response.status, responseData: response.data }
+        )).catch((error) => {
+            const errorResponse = {
+                responseStatus: error.response.status,
+                responseData: error.response.data,
+            };
+            throw errorResponse;
+        });
+    }
+
+    getDevice(userId, authToken, deviceUniqueIdentifier) {
+        const requestOptions = {
+            headers: {
+                "Content-Type": "application/json",
+                "X-User-Id": userId,
+                "X-Auth-Token": authToken,
+            },
+        };
+
+        return Axios.get(
+            `${this.API_URL}/devices/${deviceUniqueIdentifier}`,
+            requestOptions
+        ).then(response => (
+            { responseStatus: response.status, responseData: response.data }
+        )).catch((error) => {
+            const errorResponse = {
+                responseStatus: error.response.status,
+                responseData: error.response.data,
+            };
+            throw errorResponse;
+        });
+    }
+}
