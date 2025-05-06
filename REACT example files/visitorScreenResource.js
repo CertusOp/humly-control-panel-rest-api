6a7c16628b6854431313dea8da0c0100f1482dbe
sumit sharma <sumit.sharma@cloudactivelabs.com>
2025-05-06 16:33:20 +0530

    [MAT-5800] updated the structure and added a new route for visitor-screen

---
+++ REACT example files/visitorScreenResource.js
@@ -0,0 +1,34 @@
+import Axios from "axios";
+
+import RequestError from "./requestError";
+
+export default class VisitorLogsResource {
+    API_URL = "https://localhost:3002/api/v1";
+
+    getVisitorScreens(userId, authToken, queryParams) {
+        const requestOptions = {
+            headers: {
+                "X-User-Id": userId,
+                "X-Auth-Token": authToken,
+            },
+            params: {
+                pageNumber: queryParams.pageNumber,
+                pageSize: queryParams.pageSize,
+                sort: queryParams.sort,
+            },
+        };
+
+        return Axios.get(
+            `${this.API_URL}/visitor-screens`,
+            requestOptions
+        ).then(response => (
+            { responseStatus: response.status, responseData: response.data }
+        )).catch((error) => {
+            throw new RequestError(
+                error.response.data.message,
+                error.response.status,
+                error.response.data
+            );
+        });
+    }
+}
