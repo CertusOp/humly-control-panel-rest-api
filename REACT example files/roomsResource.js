6eb2d5d81019b7b498af7548430048ae90dc57e2
SulejmanVelic <velics@yahoo.com>
2025-07-23 15:30:54 +0200

    Update roomsResource.js

--- REACT example files/roomsResource.js
+++ REACT example files/roomsResource.js
@@ -4,7 +4,7 @@ import RequestError from "./requestError";
 
 export default class RoomsResource {
     const CLOUD_ID = "00000"; // Your 5 digit Humly cloud ID.
-    const API_URL = `https://${COULD_ID}/api/v1`;
+    const API_URL = `https://${CLOUD_ID}/api/v1`;
 
     getAllRooms(userId, authToken, queryParams) {
         const requestOptions = {
