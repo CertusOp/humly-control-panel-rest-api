0d8f33391f6fb84cb3ec2b8e8245fa7e4ed054ed
SulejmanVelic <velics@yahoo.com>
2025-07-23 15:30:25 +0200

    Update guideScreensResource.js

--- REACT example files/guideScreensResource.js
+++ REACT example files/guideScreensResource.js
@@ -4,7 +4,7 @@ import RequestError from "./requestError";
 
 export default class GuideScreensResource {
     const CLOUD_ID = "00000"; // Your 5 digit Humly cloud ID.
-    const API_URL = `https://${COULD_ID}/api/v1`;
+    const API_URL = `https://${CLOUD_ID}/api/v1`;;
 
     // uniqueIdentifier -wayfinding document _id or code
     getGuideScreen(userId, authToken, uniqueIdentifier) {
