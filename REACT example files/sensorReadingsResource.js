377f1d9be57e2b1d5acc6f7e05e37924d6904143
SulejmanVelic <velics@yahoo.com>
2025-07-23 15:31:12 +0200

    Update sensorReadingsResource.js

--- REACT example files/sensorReadingsResource.js
+++ REACT example files/sensorReadingsResource.js
@@ -4,7 +4,7 @@ import RequestError from "./requestError";
 
 export default class SensorReadingsResource {
     const CLOUD_ID = "00000"; // Your 5 digit Humly cloud ID.
-    const API_URL = `https://${COULD_ID}/api/v1`;
+    const API_URL = `https://${CLOUD_ID}/api/v1`;
 
     getAllSensorReadings(userId, authToken, queryParams) {
         const paramsList = [
