8767fbee67e10ea5dcacd13b1a47764adf8dabfd
SulejmanVelic <velics@yahoo.com>
2025-07-23 15:29:59 +0200

    Update desksResources.js

--- REACT example files/desksResources.js
+++ REACT example files/desksResources.js
@@ -4,7 +4,7 @@ import RequestError from "./requestError";
 
 export default class DesksResource {
     const CLOUD_ID = "00000"; // Your 5 digit Humly cloud ID.
-    const API_URL = `https://${COULD_ID}/api/v1`;
+    const API_URL = `https://${CLOUD_ID}/api/v1`;
 
     getAllDesks(userId, authToken, queryParams) {
         const requestOptions = {
