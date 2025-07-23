d4b693e8fa37dd9fab8fba52b4a4086fac9ba75d
SulejmanVelic <velics@yahoo.com>
2025-07-23 15:52:11 +0200

    Update sensorsResource.js

--- REACT example files/sensorsResource.js
+++ REACT example files/sensorsResource.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 import RequestError from "./requestError";
 
 export default class SensorsResource {
-    const CLOUD_ID = "00000"; // Your 5 digit Humly cloud ID.
+    const CLOUD_ID = "00000.humly.cloud" // Your 5 digit Humly cloud ID.
     const API_URL = `https://${CLOUD_ID}/api/v1`;
 
     getAllSensors(userId, authToken, queryParams) {
