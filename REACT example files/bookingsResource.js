f9f6b80051a676572db1f60b5766699cd17bb436
SulejmanVelic <velics@yahoo.com>
2025-07-23 15:29:40 +0200

    Update bookingsResource.js

--- REACT example files/bookingsResource.js
+++ REACT example files/bookingsResource.js
@@ -4,7 +4,7 @@ import RequestError from "./requestError";
 
 export default class BookingsResource {
     const CLOUD_ID = "00000"; // Your 5 digit Humly cloud ID.
-    const API_URL = `https://${COULD_ID}/api/v1`;
+    const API_URL = `https://${CLOUD_ID}/api/v1`;
 
     getOrganizerBookings(userId, authToken, queryParams) {
         const requestOptions = {
