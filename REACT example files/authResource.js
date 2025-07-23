2f39a0b963172071d93ded4c1b8c75b1954a55ff
SulejmanVelic <velics@yahoo.com>
2025-07-23 15:29:25 +0200

    Update authResource.js

--- REACT example files/authResource.js
+++ REACT example files/authResource.js
@@ -4,7 +4,7 @@ import RequestError from "./requestError";
 
 export default class AuthResource {
     const CLOUD_ID = "00000"; // Your 5 digit Humly cloud ID.
-    const API_URL = `https://${COULD_ID}/api/v1`;
+    const API_URL = `https://${CLOUD_ID}/api/v1`;
 
     login(username, password) {
         const requestOptions = {
